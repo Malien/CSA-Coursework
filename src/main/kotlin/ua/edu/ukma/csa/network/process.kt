876df7d315397b234b70006df7a8673e041acb6b
Yaroslav <q.link0.p@gmail.com>
2020-05-23 16:34:46 +0300

    Added packet processing functions

--- src/main/kotlin/ua/edu/ukma/csa/network/process.kt
+++ src/main/kotlin/ua/edu/ukma/csa/network/process.kt
@@ -2,6 +2,8 @@ package ua.edu.ukma.csa.network
 
 import ua.edu.ukma.csa.network.MessageType.ERR
 import ua.edu.ukma.csa.network.MessageType.OK
+import java.security.Key
+import javax.crypto.Cipher
 
 val invalidRequest = Message.Decrypted(ERR, 0)
 
@@ -11,3 +13,15 @@ fun process(message: Message.Decrypted): Message.Decrypted = when (message.type)
     OK, ERR -> invalidRequest
     else -> okResponse
 }
+
+fun handlePacket(packet: Packet<Message.Decrypted>): Packet<Message.Decrypted> = Packet(
+    clientID = 0,
+    message = process(packet.message),
+    packetID = packet.packetID
+)
+
+fun handlePacket(packet: Packet<Message.Encrypted>, key: Key, cipher: Cipher): Packet<Message.Encrypted> = Packet(
+    clientID = 0,
+    message = process(packet.message.decrypted(key, cipher)).encrypted(key, cipher),
+    packetID = packet.packetID
+)
