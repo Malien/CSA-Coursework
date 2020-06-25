646b3d116355fd20932b1d62f0e8396fb9a6d0e5
Yaroslav <q.link0.p@gmail.com>
2020-06-25 05:04:20 +0300

    Made deleteProduct not require json input with product id, but instead rely on dynamic routing
    Signed-off-by: Yaroslav <q.link0.p@gmail.com>

--- src/main/kotlin/ua/edu/ukma/csa/api/routes/deleteProduct.kt
+++ src/main/kotlin/ua/edu/ukma/csa/api/routes/deleteProduct.kt
@@ -1,26 +1,34 @@
 package ua.edu.ukma.csa.api.routes
 
-import arrow.core.Either
+import arrow.core.Left
+import arrow.core.Right
 import arrow.core.flatMap
-import ua.edu.ukma.csa.api.*
+import ua.edu.ukma.csa.api.RouteException
+import ua.edu.ukma.csa.api.RouteResponse
+import ua.edu.ukma.csa.api.authorizeHeaders
+import ua.edu.ukma.csa.api.toJsonResponse
 import ua.edu.ukma.csa.model.ModelException
 import ua.edu.ukma.csa.model.ModelSource
 import ua.edu.ukma.csa.model.ProductID
-import ua.edu.ukma.csa.network.http.HTTPResponse
 import ua.edu.ukma.csa.network.http.RouteHandler
 
 fun deleteProduct(model: ModelSource, tokenSecret: String): RouteHandler = { request ->
-    val userID = model.authorizeHeaders(request.headers, tokenSecret)
-    if (userID is Either.Left) HTTPResponse.unauthorized()
-
-    request.jsonRoute { (id): DeleteProductRequest->
-        userID.flatMap { _ ->
+    model.authorizeHeaders(request.headers, tokenSecret)
+        .flatMap {
+            try {
+                Right(request.matches["id"]!!.toInt())
+            } catch (e: NumberFormatException) {
+                Left(RouteException.UserRequest("Invalid product id"))
+            }
+        }
+        .flatMap { id ->
             model.removeProduct(ProductID(id)).mapLeft {
                 when (it) {
                     is ModelException.ProductDoesNotExist -> RouteException.NotFound(it.message)
                     else -> RouteException.serverError(it)
                 }
             }
-        }.map { RouteException.NoContent("No Content") }
-    }
+        }
+        .map { RouteResponse.Ok() }
+        .toJsonResponse()
 }
